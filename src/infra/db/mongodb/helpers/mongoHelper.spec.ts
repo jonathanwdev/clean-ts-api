dac474460e92e8a762729cfdbabb0043e89d5b27
Jonathan Souza <jonathan.wdev@gmail.com>
2020-10-03 17:01:38 -0300

    test: ensure mongodb reconnect if client is down

---
+++ src/infra/db/mongodb/helpers/mongoHelper.spec.ts
@@ -0,0 +1,19 @@
+import { MongoHelper as sut } from './mongoHelper'
+
+describe('MongoHelper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should reconnect if mongoDB is dow', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
