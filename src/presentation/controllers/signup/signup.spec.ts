b7bdc6269baa4b4136d157c33807f5c083a1ef7e
Jonathan Souza <jonathan.wdev@gmail.com>
2020-10-04 18:18:33 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -168,7 +168,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Internal server error'))
   })
 
   test('Should return 500 if AddAccount throws', async () => {
@@ -186,7 +186,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Internal server error'))
   })
 
   test('Should call addAccount with correct values', async () => {
