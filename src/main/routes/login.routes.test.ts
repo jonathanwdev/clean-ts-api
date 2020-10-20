cfe0fb87e708c2f2b0d02937c8b3c41c4425db5d
Jonathan Souza <jonathan.wdev@gmail.com>
2020-10-20 20:37:21 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -49,5 +49,15 @@ describe('login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'jonathan@mail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
