a8d0b34b462873dc8aea90ab6a6a40d3ba17e00e
Jonathan Souza <jonathan.wdev@gmail.com>
2020-11-06 14:18:28 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/loadSurveys/dbLoadSurveys.ts
@@ -0,0 +1,14 @@
+import { SurveyModel } from '../../../domains/models/survey'
+import { LoadSurveys } from '../../../domains/usecases/loadSurveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/loadSurveyRepository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return []
+  }
+}
